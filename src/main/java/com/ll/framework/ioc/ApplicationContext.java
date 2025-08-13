c8c4258b345acaa8e371f4409a1e8ef949194226
inti0 <sonin223@gmail.com>
2025-08-13 21:23:16 +0900

    v1 하드코딩

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,25 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+import java.util.HashMap;
+import java.util.Map;
 
+public class ApplicationContext {
+
+    Map<String, Object> map = new HashMap<String, Object>();
+
+    public ApplicationContext() {
+        TestPostRepository testPostRepository = new TestPostRepository();
+        TestPostService testPostService = new TestPostService(testPostRepository);
+        TestFacadePostService testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
+        map.put("testPostRepository", testPostRepository);
+        map.put("testPostService", testPostService);
+        map.put("testFacadePostService", testFacadePostService);
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        return (T) map.get(beanName);
     }
 }
